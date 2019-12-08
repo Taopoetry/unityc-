9fbaa1fe1cc0817cc70182859f120b8d034a38c5
tao99 <1257642419@qq.com>
2019-12-08 23:19:25 +0800

    游戏主体

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 控制台RPG游戏
+{
+    class Program
+    {
+
+        public static Random Random = new Random();
+
+        static void Main(string[] args)
+        {
+            
+
+            if (GameRes.LoadGameRes())
+            {
+                GameHelper.StartGame();
+
+            }
+            else
+            {
+                Console.WriteLine("游戏载入失败");
+            }
+        }
+    }
+}
